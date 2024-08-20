f00b80e6e5c775c0d770a5d0598e0381765c27b0
diozenio <d.dionisiobsl@gmail.com>
2024-08-20 16:48:26 -0300

    feat(user): Add user struct and functions

---
+++ src/include/user.c
@@ -0,0 +1,93 @@
+#include "user.h"
+
+char* getUserRole(enum UserRole role) {
+    switch (role) {
+        case ADMIN:
+            return "ADMIN";
+        case NURSE:
+            return "NURSE";
+        case DOCTOR:
+            return "DOCTOR";
+        default:
+            return "UNKNOWN";
+    }
+}
+
+enum UserRole getUserRoleByString(char* role) {
+    if (strcmp(role, "ADMIN") == 0) return ADMIN;
+    if (strcmp(role, "NURSE") == 0) return NURSE;
+    if (strcmp(role, "DOCTOR") == 0) return DOCTOR;
+    return UNKNOWN;
+}
+
+void save_user(User* user) {
+    char* data = (char*)malloc(sizeof(char));
+    sprintf(data, "%d;%s;%s;%s;%s\n", user->id, user->name, user->email, user->password, getUserRole(user->role));
+    write_file("src/bin/users.txt", data);
+    free(data);
+}
+
+User* createUser() {
+    User* user = (User*)malloc(sizeof(User));
+    separator();
+    
+    printf("Digite o nome completo do usuário: ");
+    fgets(user->name, sizeof(user->name), stdin);
+    user->name[strcspn(user->name, "\n")] = 0;
+
+    printf("Digite o email do usuário: ");
+    scanf("%s", user->email);
+    getchar();
+
+    printf("Digite a senha do usuário: ");
+    scanf("%s", user->password);
+    getchar();
+
+    user->role = ADMIN;
+    user->id = generate_random_id();
+
+    save_user(user);
+    return user;
+}
+
+void printUser(User* user) {
+    printf("ID: %d\n", user->id);
+    printf("Nome: %s\n", user->name);
+    printf("Email: %s\n", user->email);
+    printf("Senha: %s\n", user->password);
+    printf("Role: %s\n", getUserRole(user->role));
+}
+
+void parseUser(char* userData, User* user) {
+    char* token = strtok(userData, ";");
+
+    user->id = atoi(token);
+
+    token = strtok(NULL, ";");
+    strcpy(user->name, token);
+
+    token = strtok(NULL, ";");
+    strcpy(user->email, token);
+
+    token = strtok(NULL, ";");
+    strcpy(user->password, token);
+
+    token = strtok(NULL, ";");
+    user->role = getUserRoleByString(token);
+}
+
+User* findUserByEmail(char* email) {
+    char* data = read_file("src/bin/users.txt");
+    char* token = strtok(data, "\n");
+    while (token != NULL) {
+        User* user = (User*)malloc(sizeof(User));
+        char* role = (char*)malloc(sizeof(char));
+        parseUser(token, user);
+        free(role);
+        if (strcmp(user->email, email) == 0) {
+            return user;
+        }
+        token = strtok(NULL, "\n");
+    }
+    return NULL;
+}
---
+++ src/include/user.h
@@ -0,0 +1,32 @@
+#ifndef USER_H
+#define USER_H
+
+#include <stdio.h>
+#include <stdlib.h>
+#include "../utils/menu.h"
+#include "../utils/system_functions.h"
+#include "../utils/generate_random_id.h"
+#include "database.h"
+
+enum UserRole {
+  ADMIN,
+  DOCTOR,
+  NURSE,
+  UNKNOWN
+};
+
+typedef struct {
+  int id;
+  char name[100];
+  char email[50];
+  char password[50];
+  enum UserRole role;
+} User;
+
+User* createUser();
+User* findUserByEmail(char* email);
+
+char* getUserRole(enum UserRole role);
+void printUser(User* user);
+
+#endif
