3c80fce053cfc6ca1617788b5998278c7d63f3e9
diozenio <d.dionisiobsl@gmail.com>
2024-08-20 16:47:23 -0300

    refactor(database): Update read_file function to return file data

--- src/include/database.c
+++ src/include/database.c
@@ -7,10 +7,16 @@ void write_file(char *filename, char *data)
     fclose(file);
 }
 
-void read_file(char *filename)
+char* read_file(char *filename)
 {
     FILE *file = fopen(filename, "r");
+    if (file == NULL) {
+        return NULL;
+    }
+    char *data = malloc(1024);
+    fread(data, 1, 1024, file);
     fclose(file);
+    return data;
 }
 
 void delete_file(char *filename)
--- src/include/database.h
+++ src/include/database.h
@@ -6,7 +6,7 @@
 #include <string.h>
 
 void write_file(char *filename, char *data);
-void read_file(char *filename);
+char* read_file(char *filename);
 void delete_file(char *filename);
 
 #endif
