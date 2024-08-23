bddd0e5a057f287d4159162a9e34fb7d175b5b37
Pedro Henrique <phbb1@aluno.ifal.edu.br>
2024-08-22 22:35:43 -0300

    fix(admin): Add createWelcomeMessage function to resolve Makefile error

--- src/apps/admin.c
+++ src/apps/admin.c
@@ -1,23 +1,35 @@
 #include "admin.h"
 
-void create_nurse() {
+const char *WELCOME_TEMPLATE = "Bem-vindo, administrador %s!";
+
+void create_nurse()
+{
   createUser(NURSE);
 }
 
-void create_doctor() {
+void create_doctor()
+{
   createUser(DOCTOR);
 }
 
-void admin_app(User* user) {
+char *createWelcomeMessage(const char *userName)
+{
+  size_t size = strlen(WELCOME_TEMPLATE) + strlen(userName) + 1;
+  char *message = (char *)malloc(size);
+  sprintf(message, WELCOME_TEMPLATE, userName);
+  return message;
+}
+
+void admin_app(User *user)
+{
   option options[] = {
-    {"Cadastrar Enfermeira", create_nurse, NULL},
-    {"Cadastrar Médico", create_doctor, NULL},
+      {"Cadastrar Enfermeira", create_nurse, NULL},
+      {"Cadastrar Médico", create_doctor, NULL},
   };
 
   option exitOption = {"Deslogar", NULL, NULL};
 
-  char* welcomeMessage = (char*)malloc(sizeof(char));
-  sprintf(welcomeMessage, "Bem-vindo, administrador %s!", user->name);
+  char *welcomeMessage = createWelcomeMessage(user->name);
 
   menu(welcomeMessage, options, 2, &exitOption);
   free(welcomeMessage);
